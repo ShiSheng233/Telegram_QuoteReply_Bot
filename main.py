186fb1ed9e562ab4ae536cf3a4790e7be260d070
Shi_Sheng <ohayou@shisheng.icu>
2022-04-23 00:42:37 +0800

    Main: Implement program logic
    Signed-off-by: Shi_Sheng <ohayou@shisheng.icu>

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-from telegram import Update
-from telegram.ext import Updater, CommandHandler, CallbackContext, MessageHandler, Filters
-import logging
-import token
+import telegram, telegram.ext, logging
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
@@ -9,21 +6,42 @@ logging.basicConfig(
 
 logger = logging.getLogger(__name__)
 
+"""
+def command_start(update: telegram.Update, context: telegram.ext.CallbackContext) -> None:
+    user = update.effective_user
+    logger.info(f'User {user.id} tapped \'/start\'')
+    update.message.reply_markdown_v2(
+        fr'Hello {user.mention_markdown_v2()}\!'
+    )
 
-def command_start():
-    pass
+
+def command_help(update: telegram.Update, context: telegram.ext.CallbackContext) -> None:
+    user = update.effective_user
+    logger.info(f'User {user.id} tapped \'/help\'')
+    update.message.reply_markdown_v2(
+        'qwq'
+    )
+"""
 
 
-def command_help():
-    pass
+def reply_to_message(update: telegram.Update, context: telegram.ext.CallbackContext) -> None:
+    user = update.effective_user
+    logger.info(f'User {user.id} replied to a message at Chat {update.message.chat_id}')
+    update.message.reply_markdown_v2(
+        fr'`{user.full_name}` {update.message.text.replace("/", "")}了 `{update.message.reply_to_message.from_user.full_name}`'
+    )
 
 
 def main() -> None:
-    BotUpdater = Updater(token.token)
+    BotUpdater = telegram.ext.Updater("""Your BOT Token Here""")
     dispatcher = BotUpdater.dispatcher
 
-    dispatcher.add_handler(CommandHandler("start", command_start))
-    dispatcher.add_handler(CommandHandler("help", command_help))
+    # dispatcher.add_handler(telegram.ext.CommandHandler("start", command_start))
+    # dispatcher.add_handler(telegram.ext.CommandHandler("help", command_help))
+    dispatcher.add_handler(telegram.ext.MessageHandler(telegram.ext.Filters.text, reply_to_message))
+
+    BotUpdater.start_polling()
+    BotUpdater.idle()
 
 
 if __name__ == "__main__":

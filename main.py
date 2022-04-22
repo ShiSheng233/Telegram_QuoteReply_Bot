e484e0e4e2caba690d1bedf6dcebcb26deadf9a0
Shi_Sheng <ohayou@shisheng.icu>
2022-04-22 22:06:49 +0800

    Main: add 2 commands
    Signed-off-by: Shi_Sheng <ohayou@shisheng.icu>

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, CallbackContext, MessageHandler, Filters
 import logging
+import token
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
@@ -9,8 +10,20 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
-def main() -> None:
+def command_start():
     pass
+
+
+def command_help():
+    pass
+
+
+def main() -> None:
+    BotUpdater = Updater(token.token)
+    dispatcher = BotUpdater.dispatcher
+
+    dispatcher.add_handler(CommandHandler("start", command_start))
+    dispatcher.add_handler(CommandHandler("help", command_help))
 
 
 if __name__ == "__main__":
